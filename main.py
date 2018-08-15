ab3fb5e833c8987bba4c842e9a5987afd43813e7
jonhartm <jonhartm@umich.edu>
2018-08-15 03:27:13 -0400

    Print collected stats to the template

--- main.py
+++ main.py
@@ -13,6 +13,30 @@ load_dotenv()
 
 ROOT = path.dirname(path.realpath(__file__))
 
+def GetStats():
+    stats = {}
+    with sqlite.connect(path.join(ROOT, "wotcw.db")) as conn:
+        cur = conn.cursor()
+        query = '''
+        SELECT
+        	nickname,
+        	Tanks.type,
+        	SUM(battles),
+        	SUM(damage_dealt)/SUM(battles) as AvgDmg,
+        	ROUND(SUM(spotting)*1.0/SUM(battles),1) as AvgSpot
+        FROM MemberStats
+        	JOIN Tanks on Tanks.tank_id = MemberStats.tank_id
+        	JOIN Members on Members.account_id = MemberStats.account_id
+        GROUP BY Tanks.type, nickname
+        '''
+        cur.execute(query)
+        for row in cur:
+            if row[0] not in stats:
+                stats[row[0]] = {}
+            stats[row[0]][row[1]] = row[2:5]
+    return stats
+
+
 @app.route('/')
 def index():
     member_data = []
@@ -24,7 +48,7 @@ def index():
 
     return render_template(
         "index.html",
-        members=member_data
+        members=GetStats()
         )
 
 if __name__=="__main__":
@@ -39,6 +63,5 @@ if __name__=="__main__":
         elif sys.argv[1] == "-update":
             database.GetMemberTankStats(sys.argv[2])
         else:
-            pass
     else:
         app.run(debug=True)
