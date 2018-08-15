430a18f89b54bcdda358aac1ab390b3198a84847
jonhartm <jonhartm@umich.edu>
2018-08-15 16:37:50 -0400

    Redo stats - get ranks and output into nested dictionaries

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ load_dotenv()
 
 ROOT = path.dirname(path.realpath(__file__))
 
+DMG_BREAKS = [1200, 1350, 1400, 1500]
+SPOT_BREAKS = [2,4,6,8]
+ATTENDANCE_BREAKS = [.2,.4,.6,.8]
+
 def GetStats():
     stats = {}
     with sqlite.connect(path.join(ROOT, "wotcw.db")) as conn:
@@ -28,13 +32,31 @@ def GetStats():
         	JOIN Tanks on Tanks.tank_id = MemberStats.tank_id
         	JOIN Members on Members.account_id = MemberStats.account_id
         GROUP BY Tanks.type, nickname
+        HAVING battles > 0
+        ORDER BY nickname
         '''
         cur.execute(query)
         for row in cur:
+            # make sure there's a key for this player
             if row[0] not in stats:
                 stats[row[0]] = {}
-            stats[row[0]][row[1]] = row[2:5]
+            stats[row[0]][row[1]] = GetTankStats(row)
     return stats
+
+def GetTankStats(row):
+    ret_dict = {
+        "AvgDmg": row[3],
+        "AvgDmgRank": GetRank(row[3], DMG_BREAKS),
+        "Spots": row[4],
+        "SpotsRank": GetRank(row[4], SPOT_BREAKS)
+    }
+    return ret_dict
+
+def GetRank(value, breaks):
+    for rank in range(len(breaks)):
+        if value < breaks[rank]:
+            return rank + 1
+    return 5
 
 
 @app.route('/')
@@ -63,6 +85,6 @@ if __name__=="__main__":
         elif sys.argv[1] == "-update":
             database.GetMemberTankStats(sys.argv[2])
         else:
-            pass
+            print(GetStats())
     else:
         app.run(debug=True)
