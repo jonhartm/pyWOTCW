4af019b5c5b7ce625b95f9aaead66bd7a541a4f4
jonhartm <jonhartm@umich.edu>
2018-08-20 22:23:31 -0400

    added controls to swap between value and rank

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  alert("hi");
+  $(".chng-view").click(function() {
+    if (!$(this).hasClass("active")) {
+      // Toggle the active state for all buttons in this group
+      $(this).parent().children().each(function() { $(this).toggleClass("active"); });
+      $("td[class^='rank-']").each(function() {
+        swap_values($(this));
+      });
+    }
+  });
 })
+
+function swap_values(sender) {
+  var old_val = sender.find("span").text();
+  var new_val = sender.find("input").val();
+
+  sender.find("span").text(new_val);
+  sender.find("input").val(old_val);
+}
