f789cb8a0b4c3ffcf1307be04c84a7db1b3c8385
jonhartm <jonhartm@umich.edu>
2018-09-02 22:34:48 -0400

    some slightly better styles for the meta tanks page

--- static/scripts.js
+++ static/scripts.js
@@ -8,7 +8,38 @@ $(document).ready(function() {
       });
     }
   });
+
+  $( function() {
+    $( ".sortable" ).sortable({
+      connectWith: ".sortable"
+    }).disableSelection();
+  });
 })
+
+$("#btn_submit").click(function() {
+  // roll through the lists on the page and pull the rank and all the tank ids under them
+  var data = {}
+  $.each($(".tank-list-container"), function() {
+    var tier = $(this).find("h1").text();
+    data[tier] = [];
+    $.each($(this).find("input"), function() {
+      data[tier].push($(this).val());
+    })
+  });
+  console.log(data);
+
+  $.ajax({
+    url:'/meta_tanks',
+    data: data,
+    type: 'POST',
+    success: function(response) {
+      console.log(response);
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  })
+});
 
 function swap_values(sender) {
   var old_val = sender.find("span").text();
